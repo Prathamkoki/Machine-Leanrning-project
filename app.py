19ba920ae79a07142b3c7e779d89e75337e01588
Prathamkoki <prathamesh4019@gmail.com>
2022-12-19 22:10:44 +0530

    updated

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logging import logging
 
 app=Flask(__name__)#flask object
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("we are testing logging module")
     return "This is first ML project"
 
 if __name__=="__main__":
